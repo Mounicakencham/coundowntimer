2ba7b2d72d74b779917368721593d5caf624de5d
Mounicakencham <91826873+Mounicakencham@users.noreply.github.com>
2021-11-07 13:59:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var timeLeft = 60;
+var elem = document.getElementById('Timer');
+
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft == 0) {
+    clearTimeout(timerId);
+    doSomething();
+  } else {
+    elem.innerHTML = timeLeft + ' seconds remaining';
+    timeLeft--;
+  }
+}
